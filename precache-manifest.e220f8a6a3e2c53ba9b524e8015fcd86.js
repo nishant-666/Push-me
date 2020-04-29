5c84827d9b7e0940ef8bb1dbd9064c5a23a95b28
nishant666 <nishants440@gmail.com>
2020-04-29 14:02:02 +0530

    Updates

---
+++ precache-manifest.e220f8a6a3e2c53ba9b524e8015fcd86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a42ea7ebd0a8b1cb759823e702d699a",
+    "url": "/Push-me/index.html"
+  },
+  {
+    "revision": "aef029245015d7461679",
+    "url": "/Push-me/static/css/main.3c6f22e5.chunk.css"
+  },
+  {
+    "revision": "60fa03788d64742a557d",
+    "url": "/Push-me/static/js/2.6611aaef.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Push-me/static/js/2.6611aaef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef029245015d7461679",
+    "url": "/Push-me/static/js/main.6d37ef6a.chunk.js"
+  },
+  {
+    "revision": "3f2a1375543e618e2554",
+    "url": "/Push-me/static/js/runtime-main.b570f50b.js"
+  },
+  {
+    "revision": "c1d9c9beadbb3cc1191b9afeb2965908",
+    "url": "/Push-me/static/media/cold-bg.c1d9c9be.jpg"
+  }
+]);
